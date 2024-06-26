e7fe220cceace3d3cc385f9c26bc259d6785e87c
AdizDaGreat1 <adizdagreat123@gmail.com>
2024-06-26 15:59:36 +0100

    new update

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-document.getElementsByClassName("sendInfo").addEventListener("click", sendInfo(event))
+ document.querySelector("btnSend").addEventListener("click", sendInfo);
+
+let firstName = document.querySelector("firstName");
+let lastName = document.querySelector("lastName");
+let Email = document.querySelector("Email");
+let phoneNumber = document.querySelector("PhoneNumber");
+let textArea = document.querySelector("floatingTextarea");
 
 
-const firstName = document.querySelector(".firstName");
-const lastName = document.querySelector(".lastName");
-const Email = document.querySelector(".Email");
-const phoneNumber = document.querySelector(".phoneNumber");
-const textArea = document.querySelector(".textArea");
-function sendInfo(event){
-    const firstnameField = firstName.value;
-    const lastnameField = lastName.value;
-    const emailField = Email.value;
-    const phonenumberField = phoneNumber.value;
-    const textareaField = textArea.value;
+function sendInfo(){
+    const firstnameField =  firstName.value; 
+    const lastnameField =  lastName.value; 
+    const emailField =  Email.value; 
+    const phonenumberField =  phoneNumber.value; 
+    const textareaField =  textArea.value; 
     localStorage.setItem("firstName", firstnameField);
     localStorage.setItem("lastName", lastnameField);
     localStorage.setItem("Email", emailField);
     localStorage.setItem("phoneNumber", phonenumberField);
     localStorage.setItem("textArea", textareaField);
 }
+console.log(sendInfo())

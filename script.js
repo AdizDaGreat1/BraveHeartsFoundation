dce81ff94f8436dd75cc729ba57fb230255883fa
AdizDaGreat1 <adizdagreat123@gmail.com>
2024-06-26 14:48:58 +0100

    new update

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.getElementsByClassName("sendInfo").addEventListener("click", sendInfo(event))
 
+
+const firstName = document.querySelector(".firstName");
+const lastName = document.querySelector(".lastName");
+const Email = document.querySelector(".Email");
+const phoneNumber = document.querySelector(".phoneNumber");
+const textArea = document.querySelector(".textArea");
+function sendInfo(event){
+    const firstnameField = firstName.value;
+    const lastnameField = lastName.value;
+    const emailField = Email.value;
+    const phonenumberField = phoneNumber.value;
+    const textareaField = textArea.value;
+    localStorage.setItem("firstName", firstnameField);
+    localStorage.setItem("lastName", lastnameField);
+    localStorage.setItem("Email", emailField);
+    localStorage.setItem("phoneNumber", phonenumberField);
+    localStorage.setItem("textArea", textareaField);
+}
